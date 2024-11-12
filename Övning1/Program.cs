0b30513e649cf59b2ef018ed77f03332526e1e9d
Rasmus Eng <rasmus.eng@elev.ga.ntig.se>
2024-11-12 10:34:33 +0100

    dasdsa

--- Övning1/Program.cs
+++ Övning1/Program.cs
@@ -1,20 +1,29 @@
 ﻿
 
-if(6>=3){
+if (6 >= 3)
+{
     Console.WriteLine("Hello World");
 }
-    string username = Console.ReadLine();
-    string password = Console.ReadLine();
 
 
-    if (username == "kalleanka"|| password == "12345"){
-Console.WriteLine("Welcome");
-    }
-    else Console.WriteLine("Wrong username or password");
+string username = "";
+string password = "";
+while(!(username == "kalleanka" && password == "12345"))
+{
+
+    username = Console.ReadLine();
+    password = Console.ReadLine();
+if(username == "kalleanka" && password == "12345"){
+    Console.WriteLine("Welcome");
+}
+else Console.WriteLine("Wrong username or password");
+}
 
 int i = 0;
-while (i <= 32){
-Console.WriteLine("Hello World");
-i++;
+while (i <= 32)
+{
+    Console.WriteLine("Hello World");
+    i++;
 }
+
 
