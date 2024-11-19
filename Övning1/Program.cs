42ebb9741cac3c7ef6f5b1c1b62d92556b770ef2
Rasmus Eng <rasmus.eng@elev.ga.ntig.se>
2024-11-19 10:24:21 +0100

    klar klar

--- Övning1/Program.cs
+++ Övning1/Program.cs
@@ -1,48 +1,48 @@
 ﻿
 
-// if (6 >= 3)
-// {
-//     Console.WriteLine("Hello World");
-// }
+if (6 >= 3)
+{
+    Console.WriteLine("Hello World");
+}
 
 
-// string username = "";
-// string password = "";
-// while (!(username == "kalleanka" && password == "12345"))
-// {
+string username = "";
+string password = "";
+while (!(username == "kalleanka" && password == "12345"))
+{
 
-//     username = Console.ReadLine();
-//     password = Console.ReadLine();
-//     if (username == "kalleanka" && password == "12345")
-//     {
-//         Console.WriteLine("Welcome");
-//     }
-//     else Console.WriteLine("Wrong username or password");
-// }
+    username = Console.ReadLine();
+    password = Console.ReadLine();
+    if (username == "kalleanka" && password == "12345")
+    {
+        Console.WriteLine("Welcome");
+    }
+    else Console.WriteLine("Wrong username or password");
+}
 
-// int i = 0;
-// while (i <= 32)
-// {
-//     Console.WriteLine("Hello World");
-//     i++;
-// }
+int i = 0;
+while (i <= 32)
+{
+    Console.WriteLine("Hello World");
+    i++;
+}
 
 
-// int tid = 0;
-// while (tid <= 5)
-// {
-//     Console.WriteLine("Välj ett nummer");
-//     string tal = Console.ReadLine();
-//     int a = int.Parse(tal);
-//     tid++;
-//     if (a > 5)
-//     {
-//         Console.WriteLine("högre än 5!");
-//     }
+int tid = 0;
+while (tid <= 5)
+{
+    Console.WriteLine("Välj ett nummer");
+    string tal = Console.ReadLine();
+    int a = int.Parse(tal);
+    tid++;
+    if (a > 5)
+    {
+        Console.WriteLine("högre än 5!");
+    }
 
-// }
+}
 
-// while (!int.TryParse(Console.ReadLine(), out int result)) ;
+while (!int.TryParse(Console.ReadLine(), out int result)) ;
 
 
 
